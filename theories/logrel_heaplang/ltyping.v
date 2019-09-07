0aaa61d4454009b08850c68a1657580ba5ded0be
Daniël Louwrink <daniel.louwrink@gmail.com>
2019-09-07 17:28:41 +0200

    remove the requirement that semantic types be persistent

--- theories/logrel_heaplang/ltyping.v
+++ theories/logrel_heaplang/ltyping.v
@@ -5,30 +5,28 @@ From iris.heap_lang Require Import notation proofmode.
 (* The domain of semantic types: persistent Iris predicates over values *)
 Record lty Σ := Lty {
   lty_car :> val → iProp Σ;
-  lty_persistent v : Persistent (lty_car v)
 }.
-Arguments Lty {_} _%I {_}.
+Arguments Lty {_} _%I.
 Arguments lty_car {_} _ _ : simpl never.
 Bind Scope lty_scope with lty.
 Delimit Scope lty_scope with lty.
-Existing Instance lty_persistent.
 
 (* The COFE structure on semantic types *)
 Section lty_ofe.
   Context `{Σ : gFunctors}.
 
+  (* Equality of semantic types is extensional equality *)
   Instance lty_equiv : Equiv (lty Σ) := λ A B, ∀ w, A w ≡ B w.
   Instance lty_dist : Dist (lty Σ) := λ n A B, ∀ w, A w ≡{n}≡ B w.
+
+  (* OFE and COFE structure is taken from isomorphism with val -d> iProp Σ *)
   Lemma lty_ofe_mixin : OfeMixin (lty Σ).
   Proof. by apply (iso_ofe_mixin (lty_car : _ → val -d> _)). Qed.
   Canonical Structure ltyC := OfeT (lty Σ) lty_ofe_mixin.
+
   Global Instance lty_cofe : Cofe ltyC.
   Proof.
-    apply (iso_cofe_subtype' (λ A : val -d> iProp Σ, ∀ w, Persistent (A w))
-      (@Lty _) lty_car)=> //.
-    - apply _.
-    - apply limit_preserving_forall=> w.
-      by apply bi.limit_preserving_Persistent=> n ??.
+    by apply (iso_cofe (@Lty _ : (val -d> _) → ltyC) lty_car).
   Qed.
 
   Global Instance lty_inhabited : Inhabited (lty Σ) := populate (Lty inhabitant).
