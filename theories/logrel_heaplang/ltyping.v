aa035ac5152181a764cf9fbfda44d62de815fdcf
Daniël Louwrink <daniel.louwrink@gmail.com>
2019-09-08 12:00:27 +0200

    add lemmas for application/abstraction

--- theories/logrel_heaplang/ltyping.v
+++ theories/logrel_heaplang/ltyping.v
@@ -236,190 +236,28 @@ Section types_properties.
   Lemma ltyped_nat Γ (n : Z) : Γ ⊨ #n : lty_int.
   Proof. iIntros (vs) "_ /=". iApply wp_value. rewrite /lty_car /=. eauto. Qed.
 
-  (* Lemma ltyped_rec Γ f x e A1 A2 : *)
-  (*   (binder_insert f (A1 → A2)%lty (binder_insert x A1 Γ) ⊨ e : A2) -∗ *)
-  (*   Γ ⊨ (rec: f x := e) : A1 → A2. *)
-  (* Proof. *)
-  (*   iIntros "#H" (vs) "!# #HΓ /=". wp_pures. iLöb as "IH". *)
-  (*   iIntros "!#" (v) "#HA1". wp_pures. set (r := RecV f x _). *)
-  (*   iSpecialize ("H" $! (binder_insert f r (binder_insert x v vs)) with "[#]"). *)
-  (*   { iApply (env_ltyped_insert with "IH"). by iApply env_ltyped_insert. } *)
-  (*   destruct x as [|x], f as [|f]; rewrite /= -?subst_map_insert //. *)
-  (*   destruct (decide (x = f)) as [->|]. *)
-  (*   - by rewrite subst_subst delete_idemp insert_insert -subst_map_insert. *)
-  (*   - rewrite subst_subst_ne // -subst_map_insert. *)
-  (*     by rewrite -delete_insert_ne // -subst_map_insert. *)
-  (* Qed. *)
-
-  Lemma ltyped_app Γ e1 e2 A1 A2 :
-    (Γ ⊨ e1 : A1 → A2) -∗ (Γ ⊨ e2 : A1) -∗ Γ ⊨ e1 e2 : A2.
+  Lemma ltyped_app Γ1 Γ2 e1 e2 A1 A2 :
+    env_compatible Γ1 Γ2 -∗ (Γ1 ⊨ e1 : A1 → A2) -∗ (Γ2 ⊨ e2 : A1) -∗ Γ1 ∪ Γ2 ⊨ e1 e2 : A2.
   Proof.
-    iIntros "H1 H2" (vs) "HΓ /=".
-    wp_apply (wp_wand with "(H2 [HΓ //])").
-    iIntros (v) "HA".
-    wp_apply (wp_wand with "(H1 [])").
-    - admit. (* oops, our environment was eaten by the other branch, we need to split the context here *)
-    - iIntros (f) "Hf". iApply ("Hf" $! v with "HA").
-  Admitted.
-
-  Lemma ltyped_fold Γ e (B : ltyC Σ -n> ltyC Σ) :
-    (Γ ⊨ e : B (lty_rec B)) -∗ Γ ⊨ e : lty_rec B.
-  Proof.
-    iIntros "#H" (vs) "!# #HΓ /=".
-    wp_apply (wp_wand with "(H [//])"); iIntros (w) "#HB".
-    by iEval (rewrite lty_rec_unfold /lty_car /=).
-  Qed.
-  Lemma ltyped_unfold Γ e (B : ltyC Σ -n> ltyC Σ) :
-    (Γ ⊨ e : lty_rec B) -∗ Γ ⊨ rec_unfold e : B (lty_rec B).
-  Proof.
-    iIntros "#H" (vs) "!# #HΓ /=".
-    wp_apply (wp_wand with "(H [//])"); iIntros (w) "HB".
-    iEval (rewrite lty_rec_unfold /lty_car /=) in "HB". by wp_lam.
+    iIntros "Hcompat H1 H2" (vs) "HΓ /=".
+    iSpecialize ("Hcompat" with "HΓ").
+    iDestruct "Hcompat" as "[HΓ1 HΓ2]".
+    wp_apply (wp_wand with "(H2 [HΓ2 //])").
+    iIntros (v) "HA1".
+    wp_apply (wp_wand with "(H1 [HΓ1 //])").
+    iIntros (f) "Hf". iApply ("Hf" $! v with "HA1").
   Qed.
 
-  Lemma ltyped_tlam Γ e C : (∀ A, Γ ⊨ e : C A) -∗ Γ ⊨ (λ: <>, e) : ∀ A, C A.
+  Lemma ltyped_lam Γ x e A1 A2 :
+    (binder_insert x A1 Γ ⊨ e : A2) -∗
+    Γ ⊨ (λ: x, e) : A1 → A2.
   Proof.
-    iIntros "#H" (vs) "!# #HΓ /=". wp_pures.
-    iIntros "!#" (A) "/=". wp_pures. by iApply ("H" $! A).
-  Qed.
-  Lemma ltyped_tapp Γ e C A : (Γ ⊨ e : ∀ A, C A) -∗ Γ ⊨ e #() : C A.
-  Proof.
-    iIntros "#H" (vs) "!# #HΓ /=".
-    wp_apply (wp_wand with "(H [//])"); iIntros (w) "#HB". by iApply ("HB" $! A).
-  Qed.
-
-  Lemma ltyped_pack Γ e C A : (Γ ⊨ e : C A) -∗ Γ ⊨ e : ∃ A, C A.
-  Proof.
-    iIntros "#H" (vs) "!# #HΓ /=".
-    wp_apply (wp_wand with "(H [//])"); iIntros (w) "#HB". by iExists A.
-  Qed.
-  Lemma ltyped_unpack Γ e1 e2 C B :
-    (Γ ⊨ e1 : ∃ A, C A) -∗ (∀ A, Γ ⊨ e2 : C A → B) -∗ Γ ⊨ e2 e1 : B.
-  Proof.
-    iIntros "#H1 #H2" (vs) "!# #HΓ /=".
-    wp_apply (wp_wand with "(H1 [//])"); iIntros (v); iDestruct 1 as (A) "#HC".
-    wp_apply (wp_wand with "(H2 [//])"); iIntros (w) "HCB". by iApply "HCB".
+    iIntros "H" (vs) "HΓ /=". wp_pures.
+    iIntros (v) "HA1". wp_pures.
+    iSpecialize ("H" $! (binder_insert x v vs) with "[HΓ HA1]").
+    { iApply (env_ltyped_insert with "[HA1 //] [HΓ //]"). }
+    (* TODO: What does the question mark mean here? *)
+    destruct x as [|x]; rewrite /= -?subst_map_insert //.
   Qed.
 
-  Lemma ltyped_pair Γ e1 e2 A1 A2 :
-    (Γ ⊨ e1 : A1) -∗ (Γ ⊨ e2 : A2) -∗ Γ ⊨ (e1,e2) : A1 * A2.
-  Proof.
-    iIntros "#H1 #H2" (vs) "!# #HΓ /=".
-    wp_apply (wp_wand with "(H2 [//])"); iIntros (w2) "#HA2".
-    wp_apply (wp_wand with "(H1 [//])"); iIntros (w1) "#HA1".
-    wp_pures. iExists w1, w2. auto.
-  Qed.
-  Lemma ltyped_fst Γ e A1 A2 : (Γ ⊨ e : A1 * A2) -∗ Γ ⊨ Fst e : A1.
-  Proof.
-    iIntros "#H" (vs) "!# #HΓ /=".
-    wp_apply (wp_wand with "(H [//])"); iIntros (w).
-    iDestruct 1 as (w1 w2 ->) "[??]". by wp_pures.
-  Qed.
-  Lemma ltyped_snd Γ e A1 A2 : (Γ ⊨ e : A1 * A2) -∗ Γ ⊨ Snd e : A2.
-  Proof.
-    iIntros "#H" (vs) "!# #HΓ /=".
-    wp_apply (wp_wand with "(H [//])"); iIntros (w).
-    iDestruct 1 as (w1 w2 ->) "[??]". by wp_pures.
-  Qed.
-
-  Lemma ltyped_injl Γ e A1 A2 : (Γ ⊨ e : A1) -∗ Γ ⊨ InjL e : A1 + A2.
-  Proof.
-    iIntros "#H" (vs) "!# #HΓ /=".
-    wp_apply (wp_wand with "(H [//])"); iIntros (w) "#HA".
-    wp_pures. iLeft. iExists w. auto.
-  Qed.
-  Lemma ltyped_injr Γ e A1 A2 : (Γ ⊨ e : A2) -∗ Γ ⊨ InjR e : A1 + A2.
-  Proof.
-    iIntros "#H" (vs) "!# #HΓ /=".
-    wp_apply (wp_wand with "(H [//])"); iIntros (w) "#HA".
-    wp_pures. iRight. iExists w. auto.
-  Qed.
-  Lemma ltyped_case Γ e e1 e2 A1 A2 B :
-    (Γ ⊨ e : A1 + A2) -∗ (Γ ⊨ e1 : A1 → B) -∗ (Γ ⊨ e2 : A2 → B) -∗
-    Γ ⊨ Case e e1 e2 : B.
-  Proof.
-    iIntros "#H #H1 #H2" (vs) "!# #HΓ /=".
-    wp_apply (wp_wand with "(H [//])"); iIntros (w) "#[HA|HA]".
-    - iDestruct "HA" as (w1 ->) "HA". wp_pures.
-      wp_apply (wp_wand with "(H1 [//])"); iIntros (v) "#HAB". by iApply "HAB".
-    - iDestruct "HA" as (w2 ->) "HA". wp_pures.
-      wp_apply (wp_wand with "(H2 [//])"); iIntros (v) "#HAB". by iApply "HAB".
-  Qed.
-
-  Lemma ltyped_un_op Γ e op A B :
-    LTyUnOp op A B → (Γ ⊨ e : A) -∗ Γ ⊨ UnOp op e : B.
-  Proof.
-    intros ?. iIntros "#H" (vs) "!# #HΓ /=".
-    wp_apply (wp_wand with "(H [//])"); iIntros (v) "#HA".
-    iDestruct (lty_un_op with "HA") as (w ?) "#HB". by wp_unop.
-  Qed.
-  Lemma ltyped_bin_op Γ e1 e2 op A1 A2 B :
-    LTyBinOp op A1 A2 B → (Γ ⊨ e1 : A1) -∗ (Γ ⊨ e2 : A2) -∗ Γ ⊨ BinOp op e1 e2 : B.
-  Proof.
-    intros. iIntros "#H1 #H2" (vs) "!# #HΓ /=".
-    wp_apply (wp_wand with "(H2 [//])"); iIntros (v2) "#HA2".
-    wp_apply (wp_wand with "(H1 [//])"); iIntros (v1) "#HA1".
-    iDestruct (lty_bin_op with "HA1 HA2") as (w ?) "#HB". by wp_binop.
-  Qed.
-
-  Lemma ltyped_if Γ e e1 e2 B :
-    (Γ ⊨ e : lty_bool) -∗ (Γ ⊨ e1 : B) -∗ (Γ ⊨ e2 : B) -∗
-    Γ ⊨ (if: e then e1 else e2) : B.
-  Proof.
-    iIntros "#H #H1 #H2" (vs) "!# #HΓ /=".
-    iSpecialize ("H1" with "HΓ"). iSpecialize ("H2" with "HΓ").
-    wp_apply (wp_wand with "(H [//])"); iIntros (w). iDestruct 1 as ([]) "->"; by wp_if.
-  Qed.
-
-  Lemma ltyped_fork Γ e : (Γ ⊨ e : ()) -∗ Γ ⊨ Fork e : ().
-  Proof.
-    iIntros "#H" (vs) "!# #HΓ /=".
-    wp_apply wp_fork; last done. by iApply (wp_wand with "(H [//])").
-  Qed.
-
-  Lemma ltyped_alloc Γ e A : (Γ ⊨ e : A) -∗ Γ ⊨ ref e : ref A.
-  Proof.
-    iIntros "#H" (vs) "!# #HΓ /=".
-    wp_bind (subst_map _ e). iApply (wp_wand with "(H [//])"); iIntros (w) "HA".
-    iApply wp_fupd. wp_alloc l as "Hl".
-    iMod (inv_alloc (tyN .@ l) _ (∃ v, l ↦ v ∗ A v)%I with "[Hl HA]") as "#?".
-    { iExists w. iFrame. }
-    iModIntro. iExists l; auto.
-  Qed.
-  Lemma ltyped_load Γ e A : (Γ ⊨ e : ref A) -∗ Γ ⊨ ! e : A.
-  Proof.
-    iIntros "#H" (vs) "!# #HΓ /=".
-    wp_bind (subst_map _ e). iApply (wp_wand with "(H [//])"); iIntros (w).
-    iDestruct 1 as (l ->) "#?".
-    iInv (tyN.@l) as (v) "[>Hl HA]". wp_load. eauto 10.
-  Qed.
-  Lemma ltyped_store Γ e1 e2 A :
-    (Γ ⊨ e1 : ref A) -∗ (Γ ⊨ e2 : A) -∗ Γ ⊨ (e1 <- e2) : ().
-  Proof.
-    iIntros "#H1 #H2" (vs) "!# #HΓ /=".
-    wp_apply (wp_wand with "(H2 [//])"); iIntros (w2) "#HA".
-    wp_apply (wp_wand with "(H1 [//])"); iIntros (w1); iDestruct 1 as (l ->) "#?".
-    iInv (tyN.@l) as (v) "[>Hl _]". wp_store. eauto 10.
-  Qed.
-  Lemma ltyped_faa Γ e1 e2 :
-    (Γ ⊨ e1 : ref lty_int) -∗ (Γ ⊨ e2 : lty_int) -∗ Γ ⊨ FAA e1 e2 : lty_int.
-  Proof.
-    iIntros "#H1 #H2" (vs) "!# #HΓ /=".
-    wp_apply (wp_wand with "(H2 [//])"); iIntros (w2); iDestruct 1 as (n) "->".
-    wp_apply (wp_wand with "(H1 [//])"); iIntros (w1); iDestruct 1 as (l ->) "#?".
-    iInv (tyN.@l) as (v) "[>Hl Hv]"; iDestruct "Hv" as (n') "> ->".
-    wp_faa. iModIntro. eauto 10.
-  Qed.
-  Lemma ltyped_cmpxchg Γ A e1 e2 e3 :
-    LTyUnboxed A →
-    (Γ ⊨ e1 : ref A) -∗ (Γ ⊨ e2 : A) -∗ (Γ ⊨ e3 : A) -∗ Γ ⊨ CmpXchg e1 e2 e3 : A * lty_bool.
-  Proof.
-    intros. iIntros "#H1 #H2 #H3" (vs) "!# #HΓ /=".
-    wp_apply (wp_wand with "(H3 [//])"); iIntros (w3) "HA3".
-    wp_apply (wp_wand with "(H2 [//])"); iIntros (w2) "HA2".
-    iDestruct (lty_unboxed with "HA2") as %?.
-    wp_apply (wp_wand with "(H1 [//])"); iIntros (w1); iDestruct 1 as (l ->) "#?".
-    iInv (tyN.@l) as (v) "[>Hl #Hv]". wp_cmpxchg as ?|?; iModIntro;
-      (iSplitL; [by eauto 12 with iFrame | iExists _, _; eauto]).
-  Qed.
 End types_properties.
