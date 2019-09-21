a2ece03bda84c153bfdd36dd36421605036585ab
Daniël Louwrink <daniel.louwrink@gmail.com>
2019-09-21 15:10:34 +0200

    fix forall

--- theories/logrel_heaplang_sessions/rules/forall.v
+++ theories/logrel_heaplang_sessions/rules/forall.v
@@ -1,4 +1,4 @@
-From iris_examples.logrel_heaplang_sessions Require Export lty.
+From iris_examples.logrel_heaplang_sessions Require Export lty ltyping.
 From iris.heap_lang Require Export lifting metatheory.
 From iris.base_logic.lib Require Import invariants.
 From iris.heap_lang Require Import notation proofmode.
@@ -18,4 +18,17 @@ Section properties.
 
   Global Instance lty_forall_ne n : Proper (pointwise_relation _ (dist n) ==> dist n) (@lty_forall Σ _).
   Proof. solve_proper. Qed.
+
+  Lemma ltyped_tlam Γ e C : (∀ A, Γ ⊨ e : C A) -∗ Γ ⊨ (λ: <>, e) : ∀ A, C A.
+  Proof.
+    iIntros "H" (vs) "HΓ /=". wp_pures.
+    iIntros (A) "/=". wp_pures. by iApply ("H" $! A).
+  Qed.
+
+  Lemma ltyped_tapp Γ e C A : (Γ ⊨ e : ∀ A, C A) -∗ Γ ⊨ e #() : C A.
+  Proof.
+    iIntros "H" (vs) "HΓ /=".
+    wp_apply (wp_wand with "(H [HΓ //])"); iIntros (w) "HB". by iApply ("HB" $! A).
+  Qed.
+
 End properties.
