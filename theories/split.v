53a7c81dedc25f1d4ed9ed6bb89e956d7512cfb9
Daniël Louwrink <daniel.louwrink@gmail.com>
2019-11-18 13:01:41 +0100

    add lemmas about env_copy

--- theories/split.v
+++ theories/split.v
@@ -65,4 +65,45 @@ Section EnvironmentSplitting.
   (* TODO: Prove lemmas about this *)
   Definition env_copy Γ Γ' : Prop :=
     ((∀ vs, env_ltyped Γ vs -∗ □ env_ltyped Γ' vs) : iProp Σ)%I.
+
+  Lemma env_copy_empty :
+    env_copy ∅ ∅.
+  Proof.
+    iIntros (vs) "_". iModIntro.
+    rewrite /env_ltyped. simpl.
+    by rewrite big_sepM_empty.
+  Qed.
+
+  Lemma env_copy_extend x A Γ Γ' :
+    Γ !! x = None →
+    env_copy Γ Γ' →
+    env_copy (<[x := A]> Γ) Γ'.
+  Proof.
+    iIntros (HΓ Hcopy vs) "Hvs".
+    rewrite /env_ltyped.
+    rewrite (big_sepM_insert _ _ _ _ HΓ).
+    iDestruct "Hvs" as "[_ Hvs]".
+    iPoseProof (Hcopy with "Hvs") as "#Hvs2".
+    iModIntro.
+    iExact "Hvs2".
+  Qed.
+
+  Lemma env_copy_extend_copy x A Γ Γ' :
+    Γ !! x = None →
+    Γ' !! x = None →
+    LTyCopy A →
+    env_copy Γ Γ' →
+    env_copy (<[x := A]> Γ) (<[x := A]> Γ').
+  Proof.
+    iIntros (HΓx HΓ'x HcopyA Hcopy vs) "Hvs".
+    rewrite /env_ltyped.
+    rewrite (big_sepM_insert _ _ _ _ HΓx).
+    iDestruct "Hvs" as "[#HA Hvs]".
+    iPoseProof (Hcopy with "Hvs") as "#Hvs2".
+    iModIntro.
+    iApply big_sepM_insert.
+    { exact HΓ'x. }
+    iFrame "HA Hvs2".
+  Qed.
+
 End EnvironmentSplitting.
